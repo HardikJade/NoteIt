d5bb11ba70858a7f4e716a7bc36186e068ac8773
Hardik Jade <65622690+HardikJade@users.noreply.github.com>
2022-04-27 11:51:52 +0530

    Added URL

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -11,7 +11,7 @@ export const SignUp = () => {
         let password2 = document.querySelector('#s-password_2').value;
         if(username && password1 && password2 && name){
             if(password1 == password2){
-                const response = await fetch('http://localhost:5000/api/auth/signup', {
+                const response = await fetch('https://noteit123.herokuapp.com/api/auth/signup', {
                     method: 'POST',
                     headers: {
                       'Content-Type': 'application/json'
